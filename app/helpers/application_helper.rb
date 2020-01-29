152979492ab919fc818c8afeb4dab6d947ff1bc2
FelipeEnne <felipeenne@gmail.com>
2020-01-29 15:04:35 -0300

    adding the I18n to the application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+    def locale
+        I18n.locale == :en ? "Inglês" : "Português Brasil"
+    end
+
     def date_br(date_us)
         date_us.strftime("%d/%m/%Y")
     end
